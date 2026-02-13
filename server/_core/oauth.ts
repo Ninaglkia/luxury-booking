855f5241a4d2fcd7ea5d92bb8840bcb332bb8618
Ninaglkia <ninaglia089@gmail.com>
2026-02-13 11:57:43 +0100

    Use relative shared imports for Vercel API runtime

--- server/_core/oauth.ts
+++ server/_core/oauth.ts
@@ -1,4 +1,4 @@
-import { COOKIE_NAME, ONE_YEAR_MS } from "@shared/const";
+import { COOKIE_NAME, ONE_YEAR_MS } from "../../shared/const";
 import { type Request, type Response, Router } from "express";
 import * as db from "../db";
 import { getSessionCookieOptions } from "./cookies";
--- server/routers.ts
+++ server/routers.ts
@@ -1,6 +1,6 @@
 import { TRPCError } from "@trpc/server";
 import { z } from "zod";
-import { COOKIE_NAME } from "@shared/const";
+import { COOKIE_NAME } from "../shared/const";
 import { getSessionCookieOptions } from "./_core/cookies";
 import { systemRouter } from "./_core/systemRouter";
 import { publicProcedure, protectedProcedure, router } from "./_core/trpc";
